8ecc50d73c362016ccf68b8c75e94bd1e071596b
hs1427 <162937199+hs1427@users.noreply.github.com>
2024-10-14 15:24:23 +0900

    Add files via upload

--- shooting_game_3.py
+++ shooting_game_3.py
@@ -5,6 +5,8 @@ import random
 w=480
 h=640
 
+miss = 0 
+
 pad = pygame.display.set_mode((w,h)) #화면 생성
 pygame.display.set_caption("Shooting Game") #제목 설정
 #--------------------이미지로드 함수--------------------
@@ -27,13 +29,32 @@ ps=0 #전투기 스피드
 
 rlist = ['rock01','rock02','rock03','rock04','rock05','rock06','rock07','rock08','rock09','rock10']
 r, rw, rh=img_load(random.choice(rlist))
+r, rw, rh=img_load(random.choice(rlist))
 rx = random.randint(0,w-rw)
 ry = 30
+rs=2
+
+#------------------바위그리기함수------------
+def rock_init():
+    global r, rw, rh, rx, ry
+    r, rw, rh=img_load(random.choice(rlist))
+    rx = random.randint(0,w-rw)
+    ry = 30
+    rs = 2
+
+#-----------------------------------
+    
+    
+
+
+
+
 #----------------미사일 그리기------------------------------------
 
 m, mw, mh = img_load('missile')
 mx = px+pw/2-mw/2
 my = py - mh
+mlist= []   #여래개의 미사일을 담을 list
 
 
 
@@ -45,7 +66,7 @@ clock = pygame.time.Clock()
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
-            pygame.quit()    
+            pygame.quit()
             sys.exit()
 
         if event.type in [pygame.KEYDOWN]:
@@ -53,6 +74,11 @@ while True:
                 ps = -5
             elif event.key == pygame.K_RIGHT:
                 ps = +5
+            elif event.key == pygame.K_SPACE: #미사일 발사
+               
+                mx = px+pw/2-mw/2    #연발시 미사일 위치 초기화
+                my = py -mh
+                mlist.append([mx, my])  #발사할 미사일을 리스트에 추가
 
         if event.type in [pygame.KEYUP]:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -63,9 +89,27 @@ while True:
         px =0
     elif px >w-pw:
         px = w-pw
+
+
     pad.blit(bg,(0,0))
     pad.blit(p, (px,py))
-    pad.blit(m,(mx,my))
-    pad.blit(r,(rx,ry))
+
+    ry += rs
+    pad.blit(r,(rx,ry)) #바위 X 좌표 random
+
+    if ry > h:  #바위를 파괴하지 못하고 놓쳤을 경우
+        miss +=1
+        rock_init()
+
+        rs += 1
+        if rs >= 10:
+            rs = 10 
+
+
+
+    pad.blit(m,(mx,my)) #미사일 그리기
+
+
+
     pygame.display.update()
     clock.tick(60)
